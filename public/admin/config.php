39561ebf743359394a7a647a8b4a7bcc3e3fa64b
fernandofmendes <fernando.mendes@webca.com.br>
2015-06-29 18:57:28 -0300

    up files

---
+++ public/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://eventos.novatela.com.br/admin/');
+define('HTTP_CATALOG', 'http://eventos.novatela.com.br/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://eventos.novatela.com.br/admin/');
+define('HTTPS_CATALOG', 'http://eventos.novatela.com.br/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/fernandomendes/github/novatela_eventos/public/admin/');
+define('DIR_SYSTEM', '/Users/fernandomendes/github/novatela_eventos/public/system/');
+define('DIR_LANGUAGE', '/Users/fernandomendes/github/novatela_eventos/public/admin/language/');
+define('DIR_TEMPLATE', '/Users/fernandomendes/github/novatela_eventos/public/admin/view/template/');
+define('DIR_CONFIG', '/Users/fernandomendes/github/novatela_eventos/public/system/config/');
+define('DIR_IMAGE', '/Users/fernandomendes/github/novatela_eventos/public/image/');
+define('DIR_CACHE', '/Users/fernandomendes/github/novatela_eventos/public/system/cache/');
+define('DIR_DOWNLOAD', '/Users/fernandomendes/github/novatela_eventos/public/system/download/');
+define('DIR_UPLOAD', '/Users/fernandomendes/github/novatela_eventos/public/system/upload/');
+define('DIR_LOGS', '/Users/fernandomendes/github/novatela_eventos/public/system/logs/');
+define('DIR_MODIFICATION', '/Users/fernandomendes/github/novatela_eventos/public/system/modification/');
+define('DIR_CATALOG', '/Users/fernandomendes/github/novatela_eventos/public/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'novatelaeventos');
+define('DB_PREFIX', 'oc_');
---
+++ public/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://eventos.novatela.com.br/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://eventos.novatela.com.br/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/fernandomendes/github/novatela_eventos/public/catalog/');
+define('DIR_SYSTEM', '/Users/fernandomendes/github/novatela_eventos/public/system/');
+define('DIR_LANGUAGE', '/Users/fernandomendes/github/novatela_eventos/public/catalog/language/');
+define('DIR_TEMPLATE', '/Users/fernandomendes/github/novatela_eventos/public/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/fernandomendes/github/novatela_eventos/public/system/config/');
+define('DIR_IMAGE', '/Users/fernandomendes/github/novatela_eventos/public/image/');
+define('DIR_CACHE', '/Users/fernandomendes/github/novatela_eventos/public/system/cache/');
+define('DIR_DOWNLOAD', '/Users/fernandomendes/github/novatela_eventos/public/system/download/');
+define('DIR_UPLOAD', '/Users/fernandomendes/github/novatela_eventos/public/system/upload/');
+define('DIR_MODIFICATION', '/Users/fernandomendes/github/novatela_eventos/public/system/modification/');
+define('DIR_LOGS', '/Users/fernandomendes/github/novatela_eventos/public/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'novatelaeventos');
+define('DB_PREFIX', 'oc_');
