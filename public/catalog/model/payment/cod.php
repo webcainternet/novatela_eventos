9c2e75eaa3170269be36c666e720ed1a26ac8788
fernandofmendes <fernando.mendes@webca.com.br>
2015-07-14 03:13:24 -0300

    back

--- public/catalog/model/payment/cod.php
+++ public/catalog/model/payment/cod.php
@@ -5,13 +5,8 @@ class ModelPaymentCOD extends Model {
 
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('cod_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
-		if ($this->config->get('cod_total') = 0) {
-			$status = true;
-		} else {
+		if ($this->config->get('cod_total') > 0 && $this->config->get('cod_total') > $total) {
 			$status = false;
-		}			
-		/* if ($this->config->get('cod_total') > 0 && $this->config->get('cod_total') > $total) {
-			$status = false; 
 		} elseif (!$this->config->get('cod_geo_zone_id')) {
 			$status = true;
 		} elseif ($query->num_rows) {
@@ -19,7 +14,6 @@ class ModelPaymentCOD extends Model {
 		} else {
 			$status = false;
 		}
-		*/
 
 		$method_data = array();
 
